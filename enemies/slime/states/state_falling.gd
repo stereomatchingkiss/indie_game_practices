6dc82e6753bba0be2b2cfde1b27e43e966a76e06
stereomatchingkiss <thamngapwei@gmail.com>
2025-05-30 14:40:17 +0800

    remove useless variable

--- enemies/slime/states/state_falling.gd
+++ enemies/slime/states/state_falling.gd
@@ -3,8 +3,6 @@ extends State
 @export
 var state_moving: State
 
-var move_velocity := Vector3(-1,0,0)
-
 func process_physics(delta: float) -> State:
 	if not parent.is_on_floor():
 		#print_debug(parent.name, " = falling")
