42632b9bd3a8de3887e0231cbe0e8c62e8f87bb8
stereomatchingkiss <thamngapwei@gmail.com>
2025-05-31 12:06:34 +0800

    first commit

---
+++ player/auxiliary_functions.gd
@@ -0,0 +1,26 @@
+class_name AuxiliaryFunctionsPlayer
+
+extends Node
+
+const SPEED = 5.0
+
+@export
+var state_moving : State
+
+@onready
+var avatar_sample_b := %AvatarSample_B
+@onready
+var camera_controller := %CameraController
+
+func get_input_dir() -> Vector2:
+	return Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+func move_character(player : Player):
+	var input_dir := get_input_dir()
+	var direction = (camera_controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		player.velocity.x = direction.x * SPEED
+		player.velocity.z = direction.z * SPEED
+	else:
+		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
+		player.velocity.z = move_toward(player.velocity.z, 0, SPEED)
