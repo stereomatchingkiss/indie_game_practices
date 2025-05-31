a5784f652b3a7b3a949fe0c0b429565808c2f70c
stereomatchingkiss <thamngapwei@gmail.com>
2025-05-31 15:06:47 +0800

    do not change jump state to falling state when player is falling after jump

--- player/states/state_jump.gd
+++ player/states/state_jump.gd
@@ -1,14 +1,23 @@
 extends State
 
+const JUMP_VELOCITY = 8.5
+
 @export
-var state_jump : State
+var state_moving : State
 
-func process_input(event: InputEvent) -> State:
-	if Input.is_action_just_pressed('ui_accept') and parent.is_on_floor():
-		return state_jump
-	return null
+@onready
+var aux_func := %AuxiliaryFunctions
 
-func process_physics(delta: float) -> State:
-	
-	
+func process_physics(delta: float) -> State:	
+	#print("jumping")
+	if parent.is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			parent.velocity.y = JUMP_VELOCITY
+		else:
+			return state_moving	
+	else:
+		aux_func.move_character(parent)
+		
+	parent.move_and_slide()
+
 	return null
