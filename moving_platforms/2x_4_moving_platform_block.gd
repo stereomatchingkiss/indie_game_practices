b078d9660c5e6a50d5a4ac8f3f0235bae7e31606
stereomatchingkiss <thamngapwei@gmail.com>
2025-06-04 00:22:26 +0800

    use another solution to move the platform, this solution only need to create Tweet once

--- moving_platforms/2x_4_moving_platform_block.gd
+++ moving_platforms/2x_4_moving_platform_block.gd
@@ -10,15 +10,20 @@ var move_duration := 5.0
 @export
 var end_point_delay := 0.5
 
+var current_tween : Tween = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	move()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func move():
-	while(1):
-		print_debug("call moving platform loop =", Time.get_unix_time_from_system())
-		var tween = create_tween()
-		tween.tween_property(self, "position", pos_end, move_duration).set_delay(end_point_delay)
-		tween.tween_property(self, "position", pos_begin, move_duration).set_delay(end_point_delay)
-		await tween.finished
+	print("call move_2, ", Time.get_unix_time_from_system())
+	if current_tween and current_tween.is_running():
+		current_tween.kill()
+		
+	current_tween = create_tween().set_loops() # 设置为无限循环
+	
+	current_tween.tween_property(self, "position", pos_end, move_duration)	
+	current_tween.tween_interval(end_point_delay)	
+	current_tween.tween_property(self, "position", pos_begin, move_duration)	
+	current_tween.tween_interval(end_point_delay)
