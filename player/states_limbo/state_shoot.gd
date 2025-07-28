9a6db788c8ba5e9376601654004d91cd32dab015
stereomatchingkiss <thamngapwei@gmail.com>
2025-07-28 11:50:42 +0800

    fix bug, wrong arrow directiion

--- player/states_limbo/state_shoot.gd
+++ player/states_limbo/state_shoot.gd
@@ -8,22 +8,26 @@ var animation_player : AnimationPlayer
 
 const fireball = preload("res://bullets/bullet_standard.tscn")
 
+func _spawn_bullet() -> void:
+	var fnode = fireball.instantiate()
+	agent.get_parent().add_child(fnode)	
+	print_debug("shoot state player direction = ", agent.player_direction())
+	if agent.player_direction() < 90:		
+		fnode.global_position = agent.global_position + Vector3(0.5, 0.5, 0)
+		fnode.player_direction = Vector3(1, 0, 0)		
+		fnode.rotation_degrees.y = -90
+	else:
+		fnode.global_position = agent.global_position + Vector3(-0.5, 0.5, 0)
+		fnode.player_direction = Vector3(-1, 0, 0)
+		fnode.rotation_degrees.y = 90
+
 # Called when the node enters the scene tree for the first time.
 func _enter() -> void:
 	print_debug("limbo : enter state_shoot, ", Time.get_unix_time_from_system())
 	agent.input_cache.reset_shoot()
 	animation_player.play("1H_Ranged_Shooting")
 			
-	var fnode = fireball.instantiate()
-	agent.get_parent().add_child(fnode)	
-	if agent.player_direction() < 0:
-		fnode.global_position = agent.global_position + Vector3(-0.5, 0.5, 0)
-		fnode.player_direction = Vector3(-1, 0, 0)		
-		fnode.rotation_degrees.y = 90
-	else:
-		fnode.global_position = agent.global_position + Vector3(0.5, 0.5, 0)
-		fnode.player_direction = Vector3(1, 0, 0)
-		fnode.rotation_degrees.y = -90
+	_spawn_bullet()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _update(delta: float) -> void:
 	#print_debug("limbo : state_shoot, ", Time.get_unix_time_from_system())	
