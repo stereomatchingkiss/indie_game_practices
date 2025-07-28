d1667260f5e203bceed93d952bccd2fa7cea08ec
stereomatchingkiss <thamngapwei@gmail.com>
2025-07-28 10:28:42 +0800

    do not allow user to shoot when moving

--- player/states_limbo/state_ground_moving.gd
+++ player/states_limbo/state_ground_moving.gd
@@ -10,16 +10,18 @@ var animation_player : AnimationPlayer
 func _enter() -> void:
 	animation_player.play("Walking_A")
 
-func _update(delta: float) -> void:
+func _update(delta: float) -> void:	
 	if not agent.is_on_floor():
 		print_debug("Limbo: state_ground_moving to ground_to_air, ", Time.get_unix_time_from_system())		
 		get_root().dispatch(&"ground_to_air")
-	elif agent.input_cache.get_input_direction() == Vector2():
-		print_debug("Limbo: state_ground_moving to ground_to_idle, ", Time.get_unix_time_from_system())		
-		get_root().dispatch(&"ground_to_idle")
-	elif agent.input_cache.get_input_direction() != Vector2():
-		print_debug("Limbo state_ground_moving, ", agent.input_cache.get_input_direction(), ",", Time.get_unix_time_from_system())
-		if agent.input_cache.get_jump():
-			get_root().dispatch(&"ground_to_jumping")
-		else:
-			aux_func.move_character(agent)
+	else:
+		agent.input_cache.reset_shoot()
+		if agent.input_cache.get_input_direction() == Vector2():
+			print_debug("Limbo: state_ground_moving to ground_to_idle, ", Time.get_unix_time_from_system())		
+			get_root().dispatch(&"ground_to_idle")
+		elif agent.input_cache.get_input_direction() != Vector2():
+			print_debug("Limbo state_ground_moving, ", agent.input_cache.get_input_direction(), ",", Time.get_unix_time_from_system())
+			if agent.input_cache.get_jump():
+				get_root().dispatch(&"ground_to_jumping")
+			else:
+				aux_func.move_character(agent)
