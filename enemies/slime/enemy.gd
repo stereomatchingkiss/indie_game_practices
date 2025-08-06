a540a1f75217c324e09854f205a9a3f13fb325d4
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-06 15:32:40 +0800

    fix bug, player could not be killed

--- enemies/slime/enemy.gd
+++ enemies/slime/enemy.gd
@@ -34,7 +34,7 @@ func _physics_process(delta: float) -> void:
 func _on_area_attack_player_body_entered(body: Node3D) -> void:
 	if body.name == "player":
 		print_debug(body.name, " = Player enter enemy attack area")
-		get_tree().change_scene_to_file("res://stage1/stage_1.tscn")		
+		get_tree().change_scene_to_file("res://stages/1/stage_1.tscn")
 
 func _on_area_step_on_body_entered(body: Node3D) -> void:
 	if body.name == "player":
