7a027be829d4f5ddd54edf9fc6523ad5660d074e
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-11 10:17:14 +0800

    first commit

---
+++ sounds/sound_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func play_jump():
+	$sound_effects/jump.play()
+	
+func play_land():
+	$sound_effects/land.play()
+	
+func play_shoot_standard_bullet():
+	$sound_effects/shoot_standard_bullet.play()
