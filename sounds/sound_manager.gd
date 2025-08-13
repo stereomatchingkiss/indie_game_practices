0ed99cf98133cb91c7822e7db0ab3807ccb59cb5
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-13 14:36:17 +0800

    add new function to preload sounds, avoid delay when the sound first play

--- sounds/sound_manager.gd
+++ sounds/sound_manager.gd
@@ -1,10 +1,16 @@
 extends Node
 
+func init():
+	$sound_effects/hit_enemy_by_arrow.volume_db = -80
+	play_hit_enemy_by_arrow()
+	await $sound_effects/hit_enemy_by_arrow.finished
+	$sound_effects/hit_enemy_by_arrow.volume_db = 0
+
 func play_dead_player():
 	$sound_effects/dead_player.play()
 	
 func play_hit_enemy_by_arrow():
-	$sound_effects/hit_enemy_by_arrow.play()
+	$sound_effects/hit_enemy_by_arrow.play(0.13)
 
 func play_jump():
 	$sound_effects/jump.play()
