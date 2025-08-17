e702d625cf20e320da68b0a474c81cd249fdd611
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-18 00:49:47 +0800

    kill enemy if hit by two times

--- enemies/alien/alien.gd
+++ enemies/alien/alien.gd
@@ -2,8 +2,6 @@ extends CharacterBody3D
 
 @export var move_velocity : Vector3
 
-@onready var hp_ := 2
-
 @onready var animations_ := $AnimationPlayer
 
 @onready var state_machine_utils_: Node = %state_machine_utils
@@ -11,7 +9,8 @@ extends CharacterBody3D
 @onready var limbo_hsm_: LimboHSM = $LimboHSM
 @onready var state_get_hit_: LimboState = $LimboHSM/state_get_hit
 
-var get_hit := false
+var get_hit_ := false
+@onready var hp_ := 2
 
 func attack_player_mask_is_active() -> bool:
 	return $AreaAttackPlayer.get_collision_mask_value(1)
@@ -20,11 +19,23 @@ func disable_attack_player_mask():
 	$AreaAttackPlayer.set_collision_mask_value(1, false)
 	
 func disable_player_step_on_mask():
-	$AreaStepOn.set_collision_mask_value(1, false)	
-
+	$AreaStepOn.set_collision_mask_value(1, false)
+	
+func get_hp() -> int:
+	return hp_;
+	
+func get_get_hit() -> bool:
+	return get_hit_
+	
+func set_get_hit(val : bool):
+	get_hit_ = val
+	
+func reduce_hp(val : int):
+	hp_ -= val
+	
 func _ready() -> void:	
 	self.velocity = move_velocity
-	state_machine_utils_.init(self)
+	state_machine_utils_.init(self)	
 	
 func _physics_process(delta: float) -> void:
 	move_and_slide()
@@ -34,6 +45,7 @@ func _on_area_attack_player_body_entered(body: Node3D) -> void:
 
 func _on_area_hit_by_bullet_area_entered(area: Area3D) -> void:	
 	if area.get_type_name() == "bullet":
-		get_hit = true
+		get_hit_ = true
+		state_get_hit_.bullet_damage_queue.push_back(area.get_bullet_damage())
 		limbo_hsm_.dispatch(&"get_hit")
 		area.queue_free()
--- enemies/alien/states_limbo/state_get_hit.gd
+++ enemies/alien/states_limbo/state_get_hit.gd
@@ -1,9 +1,12 @@
 extends LimboState
 
 @export var animation_player_ : AnimationPlayer
+
 @onready var timer_get_hit_: Timer = %timer_get_hit
 
 var velocity_cache_ : Vector3
+
+var bullet_damage_queue : Array[int]
 
 func  _ready() -> void:
 	print_debug("get hit connect")
@@ -12,21 +15,30 @@ func  _ready() -> void:
 func _change_state() -> void:
 	print_debug("enemy hit to moving time out, vcahce = ", velocity_cache_)
 	agent.velocity = velocity_cache_
+	if not bullet_damage_queue.is_empty():
+		agent.reduce_hp(bullet_damage_queue.back())
+		bullet_damage_queue.pop_back()
+		if agent.get_hp() <= 0:
+			agent.queue_free()
 	get_root().dispatch(&"hit_to_moving")
 
 func _enter() -> void:
 	pass
 
+func _get_hit() -> void:
+	if timer_get_hit_.is_stopped() or agent.get_get_hit():
+		print_debug("enemy hit")
+		animation_player_.play("HitReact")
+		SoundManager.play_hit_enemy_by_arrow()
+		if agent.velocity != Vector3():
+			velocity_cache_ = agent.velocity
+		agent.velocity = Vector3()
+		agent.set_get_hit(false)
+		timer_get_hit_.start(0.3)
+
 func _update(delta: float) -> void:	
 	if not agent.is_on_floor():
 		agent.velocity += agent.get_gravity() * delta
+		_get_hit()
 	else:
-		if timer_get_hit_.is_stopped() or agent.get_hit:
-			print_debug("enemy hit")
-			animation_player_.play("HitReact")
-			SoundManager.play_hit_enemy_by_arrow()
-			if agent.velocity != Vector3():
-				velocity_cache_ = agent.velocity
-			agent.velocity = Vector3()
-			agent.get_hit = false
-			timer_get_hit_.start(0.3)
+		_get_hit()
