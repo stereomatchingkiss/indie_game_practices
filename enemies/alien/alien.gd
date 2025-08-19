39b6d321da71f64cc5c30724383d8d2f83cbd822
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-19 08:16:35 +0800

    1. Add function to convert enemy to ball 2. Remove useless function 3. Better naming convention, easier to maintain

--- enemies/alien/alien.gd
+++ enemies/alien/alien.gd
@@ -3,23 +3,36 @@ extends CharacterBody3D
 @export var move_velocity : Vector3
 
 @onready var animations_ := $AnimationPlayer
+@onready var limbo_hsm_: LimboHSM = $LimboHSM
 
 @onready var state_machine_utils_: Node = %state_machine_utils
-
-@onready var limbo_hsm_: LimboHSM = $LimboHSM
 @onready var state_get_hit_: LimboState = $LimboHSM/state_get_hit
 
-var get_hit_ := false
+@onready var get_hit_ := false
 @onready var hp_ := 2
 
+@onready var area_attack_player_: Area3D = $AreaAttackPlayer
+@onready var area_hit_by_bullet_: Area3D = $AreaHitByBullet
+@onready var bullet_ball_: Area3D = $bullet_ball
+@onready var character_armature_: Node3D = $CharacterArmature
+@onready var enemy_: CharacterBody3D = $"."
+
 func attack_player_mask_is_active() -> bool:
-	return $AreaAttackPlayer.get_collision_mask_value(1)
+	return area_attack_player_.get_collision_mask_value(1)
+	
+func change_to_bullet_ball() -> void:
+	if not bullet_ball_.visible:
+		self.velocity = Vector3()
+		enemy_.set_collision_mask_value(2, false)
+		enemy_.set_collision_mask_value(6, false)
+		area_attack_player_.set_collision_mask_value(1, false)
+		area_hit_by_bullet_.set_collision_mask_value(7, false)
+
+		bullet_ball_.visible = true
+		character_armature_.visible = false
 	
 func disable_attack_player_mask():
-	$AreaAttackPlayer.set_collision_mask_value(1, false)
-	
-func disable_player_step_on_mask():
-	$AreaStepOn.set_collision_mask_value(1, false)
+	area_attack_player_.set_collision_mask_value(1, false)
 	
 func get_hp() -> int:
 	return hp_;
@@ -35,7 +48,8 @@ func reduce_hp(val : int):
 	
 func _ready() -> void:	
 	self.velocity = move_velocity
-	state_machine_utils_.init(self)	
+	state_machine_utils_.init(self)
+	bullet_ball_.visible = false
 	
 func _physics_process(delta: float) -> void:
 	move_and_slide()
@@ -48,4 +62,3 @@ func _on_area_hit_by_bullet_area_entered(area: Area3D) -> void:
 		get_hit_ = true
 		state_get_hit_.bullet_damage_queue.push_back(area.get_bullet_damage())
 		limbo_hsm_.dispatch(&"get_hit")
-		area.queue_free()
