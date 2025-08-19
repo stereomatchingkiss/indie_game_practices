b62515a7e87179f784fb2abae12002f31c5f0565
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-19 22:47:29 +0800

    fix bug, enemy stop moving even it did not convert to ball

--- enemies/alien/states_limbo/state_get_hit.gd
+++ enemies/alien/states_limbo/state_get_hit.gd
@@ -17,18 +17,18 @@ func _change_state() -> void:
 	if agent.get_hp() <= 0:
 		get_root().dispatch(&"hit_to_ball")
 	else:
+		agent.velocity = velocity_cache_
 		get_root().dispatch(&"hit_to_moving")
 
 func _enter() -> void:
 	pass
 
 func _get_hit() -> void:
-	if timer_get_hit_.is_stopped() or agent.get_get_hit():
+	if agent.get_get_hit():
 		print_debug("enemy hit")
 		animation_player_.play("HitReact")
 		SoundManager.play_hit_enemy_by_arrow()
 		
-		agent.velocity = velocity_cache_
 		if not bullet_damage_queue.is_empty():
 			agent.reduce_hp(bullet_damage_queue.back())
 			bullet_damage_queue.pop_back()
@@ -48,6 +48,5 @@ func _get_hit() -> void:
 func _update(delta: float) -> void:	
 	if not agent.is_on_floor():
 		agent.velocity += agent.get_gravity() * delta
-		_get_hit()
-	else:
-		_get_hit()
+	
+	_get_hit()
