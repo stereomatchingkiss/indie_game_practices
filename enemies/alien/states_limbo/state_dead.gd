68ed2fc385afdb49f07c65fa3c539b93a70014c4
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-26 00:50:53 +0800

    first commit

---
+++ enemies/alien/states_limbo/state_dead.gd
@@ -0,0 +1,12 @@
+extends LimboState
+
+@export var animation_player_ : AnimationPlayer
+
+func _enter() -> void:
+	agent.velocity = Vector3()
+	agent.set_collision_mask_value(4, false)
+	SoundManager.play_dead_enemy()
+	animation_player_.play("Death")
+
+func _update(delta: float) -> void:	
+	pass
