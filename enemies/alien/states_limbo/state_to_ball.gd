86044a820acf2687e1798ba7778f090e172abb9f
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-27 13:43:12 +0800

    kill the ball if it hit on the ground 5 times

--- enemies/alien/states_limbo/state_to_ball.gd
+++ enemies/alien/states_limbo/state_to_ball.gd
@@ -1,5 +1,7 @@
 extends LimboState
 
+var bounce_limit_ := 5
+
 func _enter() -> void:
 	agent.change_to_bullet_ball()
 	agent.velocity.y = 0.5
@@ -8,11 +10,16 @@ func _update(delta: float) -> void:
 	if agent.get_collision_mask_value(4):
 		var collide = agent.move_and_collide(agent.velocity * delta)
 		if collide:
-			#print_debug("collide velocity = ", agent.velocity)
-			var reflect = collide.get_remainder().bounce(collide.get_normal())
-			agent.velocity.z = 0
-			agent.velocity = agent.velocity.bounce(collide.get_normal())
-			agent.velocity = agent.velocity.normalized() * agent.get_bouncing_speed()
-			agent.move_and_collide(reflect)
+			if collide.get_collider().get_class() != "CharacterBody3D":
+				bounce_limit_ -= 1
+			if bounce_limit_ > 0:
+				#print_debug("collide velocity = ", agent.velocity)
+				var reflect = collide.get_remainder().bounce(collide.get_normal())
+				agent.velocity.z = 0
+				agent.velocity = agent.velocity.bounce(collide.get_normal())
+				agent.velocity = agent.velocity.normalized() * agent.get_bouncing_speed()
+				agent.move_and_collide(reflect)
+			else:
+				agent.queue_free()
 	else:
 		agent.move_and_slide()
