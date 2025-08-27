19af47ebfb595220d62e23c1a2394eb304f6894c
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-27 12:32:26 +0800

    do not bounce the ball if it float to the top

--- enemies/alien/states_limbo/state_to_ball.gd
+++ enemies/alien/states_limbo/state_to_ball.gd
@@ -5,11 +5,14 @@ func _enter() -> void:
 	agent.velocity.y = 0.5
 
 func _update(delta: float) -> void:	
-	var collide = agent.move_and_collide(agent.velocity * delta)
-	if collide:
-		#print_debug("collide velocity = ", agent.velocity)
-		var reflect = collide.get_remainder().bounce(collide.get_normal())
-		agent.velocity.z = 0
-		agent.velocity = agent.velocity.bounce(collide.get_normal())
-		agent.velocity = agent.velocity.normalized() * agent.get_bouncing_speed()
-		agent.move_and_collide(reflect)
+	if agent.get_collision_mask_value(4):
+		var collide = agent.move_and_collide(agent.velocity * delta)
+		if collide:
+			#print_debug("collide velocity = ", agent.velocity)
+			var reflect = collide.get_remainder().bounce(collide.get_normal())
+			agent.velocity.z = 0
+			agent.velocity = agent.velocity.bounce(collide.get_normal())
+			agent.velocity = agent.velocity.normalized() * agent.get_bouncing_speed()
+			agent.move_and_collide(reflect)
+	else:
+		agent.move_and_slide()
