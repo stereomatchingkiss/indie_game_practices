576e4b7d9f702f8629820a5802340bee0089fbd9
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-27 13:20:15 +0800

    Do not let player bounce the ball if player vector2 is zero

--- enemies/alien/alien.gd
+++ enemies/alien/alien.gd
@@ -66,7 +66,7 @@ func reduce_hp(val : int):
 	
 func _on_boby_entered_bullet_ball(body : Node3D):
 	var tname : StringName = body.get_type_name()
-	if tname == &"player":
+	if tname == &"player" and Vector2(body.velocity.x, body.velocity.y) != Vector2():
 		print_debug("player enter bullet ball, ", body.get_type_name())
 		enemy_.set_collision_mask_value(6, false)
 		enemy_.set_collision_mask_value(4, true)
