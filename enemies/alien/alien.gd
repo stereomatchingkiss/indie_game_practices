bb674c416ea4b4d7d7f9289c99663b933363ae86
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-29 09:44:49 +0800

    use inheritance to reduce duplicate codes

--- enemies/alien/alien.gd
+++ enemies/alien/alien.gd
@@ -1,69 +1,10 @@
-extends CharacterBody3D
-
-@export var move_velocity : Vector3
-@export var hp_ := 2
-
-@export var limbo_hsm_: LimboHSM
-
-@export var state_machine_utils_: Node
-@export var state_get_hit_: LimboState
-
-@export var area_attack_player_: Area3D
-@export var bullet_ball_: Area3D
-
-@export var bullet_ball_utils_: Node
-
-@onready var get_hit_ := false
-
-func attack_player_mask_is_active() -> bool:
-	return area_attack_player_.get_collision_mask_value(1)
-
-func change_state(state_name : StringName) -> void:
-	limbo_hsm_.dispatch(state_name)
-
-func change_to_bullet_ball() -> void:
-	bullet_ball_utils_.change_to_bullet_ball()
-
-func disable_attack_player_mask():
-	area_attack_player_.set_collision_mask_value(1, false)
-
-func get_bouncing_speed() -> float:
-	return 10
-
-func get_is_bullet_ball() -> bool:
-	return bullet_ball_.visible
-
-func get_hp() -> int:
-	return hp_;
-	
-func get_get_hit() -> bool:
-	return get_hit_
-	
-func get_type_name() -> StringName:
-	return &"enemy"
+extends EnemyBase
 	
 func get_body_name() -> StringName:
 	return &"alien"
 
 func recover_to_enemy() -> void:
 	bullet_ball_utils_.recover_to_enemy()
-
-func reduce_hp(val : int):
-	hp_ -= val
-
-func set_get_hit(val : bool):
-	get_hit_ = val
-
-func _ready() -> void:	
-	velocity = move_velocity
-	state_machine_utils_.init(self)
-	bullet_ball_.visible = false
-	
-	bullet_ball_.boby_entered.connect(bullet_ball_utils_.on_boby_entered_bullet_ball)
-	
-func _physics_process(delta: float) -> void:
-	if not bullet_ball_.visible:
-		move_and_slide()
 
 func _on_area_attack_player_body_entered(body: Node3D) -> void:
 	SoundManager.play_dead_player()
--- enemies/dog/dog.gd
+++ enemies/dog/dog.gd
@@ -1,69 +1,7 @@
-extends CharacterBody3D
+extends EnemyBase
 
-@export var move_velocity : Vector3
-@export var hp_ := 2
-
-@export var limbo_hsm_: LimboHSM
-
-@export var state_machine_utils_: Node
-@export var state_get_hit_: LimboState
-
-@export var area_attack_player_: Area3D
-@export var bullet_ball_: Area3D
-
-@export var bullet_ball_utils_: Node
-
-@onready var get_hit_ := false
-
-func attack_player_mask_is_active() -> bool:
-	return area_attack_player_.get_collision_mask_value(1)
-
-func change_state(state_name : StringName) -> void:
-	limbo_hsm_.dispatch(state_name)
-
-func change_to_bullet_ball() -> void:
-	bullet_ball_utils_.change_to_bullet_ball()
-
-func disable_attack_player_mask():
-	area_attack_player_.set_collision_mask_value(1, false)
-
-func get_bouncing_speed() -> float:
-	return 10
-
-func get_is_bullet_ball() -> bool:
-	return bullet_ball_.visible
-
-func get_hp() -> int:
-	return hp_;
-	
-func get_get_hit() -> bool:
-	return get_hit_
-	
-func get_type_name() -> StringName:
-	return &"enemy"
-	
 func get_body_name() -> StringName:
-	return &"alien"
-
-func recover_to_enemy() -> void:
-	bullet_ball_utils_.recover_to_enemy()
-
-func reduce_hp(val : int):
-	hp_ -= val
-
-func set_get_hit(val : bool):
-	get_hit_ = val
-
-func _ready() -> void:	
-	velocity = move_velocity
-	state_machine_utils_.init(self)
-	bullet_ball_.visible = false
-	
-	bullet_ball_.boby_entered.connect(bullet_ball_utils_.on_boby_entered_bullet_ball)
-	
-func _physics_process(delta: float) -> void:
-	if not bullet_ball_.visible:
-		move_and_slide()
+	return &"dog"
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "Death":
