4456c4ca977baa0c64297567176d87ea7e9ef4f5
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-29 17:44:24 +0800

    easier to read and remove duplicate call on time stop

--- enemies/states_limbo/state_to_ball.gd
+++ enemies/states_limbo/state_to_ball.gd
@@ -13,25 +13,25 @@ func _enter() -> void:
 	agent.velocity.y = 0.7
 	
 func _recover_to_enemy() -> void:
-	agent.recover_to_enemy()
-
+	if not agent.get_collision_mask_value(4):
+		agent.recover_to_enemy()
+	
+func _bounce_ball(collide :  KinematicCollision3D) ->void:
+	var reflect = collide.get_remainder().bounce(collide.get_normal())
+	agent.velocity.z = 0
+	agent.velocity = agent.velocity.bounce(collide.get_normal())
+	agent.velocity = agent.velocity.normalized() * agent.get_bouncing_speed()
+	agent.move_and_collide(reflect)
+	
 func _update(delta: float) -> void:	
 	if agent.get_collision_mask_value(4):
-		timer_reover_to_enemy_.stop()
 		var collide = agent.move_and_collide(agent.velocity * delta)
 		if collide:
 			if collide.get_collider().get_class() != "CharacterBody3D":
 				bounce_limit_ -= 1
 				if bounce_limit_ > 0:
-					#print_debug("collide velocity = ", agent.velocity)
-					var reflect = collide.get_remainder().bounce(collide.get_normal())
-					agent.velocity.z = 0
-					agent.velocity = agent.velocity.bounce(collide.get_normal())
-					agent.velocity = agent.velocity.normalized() * agent.get_bouncing_speed()
-					agent.move_and_collide(reflect)
+					_bounce_ball(collide)
 				else:
 					agent.queue_free()
-			else:
-				agent.queue_free()
 	else:
 		agent.move_and_slide()
