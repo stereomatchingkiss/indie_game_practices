1b650e418c0d92637b2444bd61f50c0cf9d45a2a
stereomatchingkiss <thamngapwei@gmail.com>
2025-08-29 17:43:22 +0800

    add debug message

--- enemies/states_limbo/state_dead.gd
+++ enemies/states_limbo/state_dead.gd
@@ -3,6 +3,7 @@ extends LimboState
 @export var animation_player_ : AnimationPlayer
 
 func _enter() -> void:
+	print_debug(agent.name, " enter dead state")
 	agent.velocity = Vector3()
 	agent.set_collision_mask_value(4, false)
 	SoundManager.play_dead_enemy()
