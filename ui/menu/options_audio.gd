3d540ee0d0ad8eefb4f9de032c8d495f6b4a01b2
stereomatchingkiss <thamngapwei@gmail.com>
2025-11-13 20:00:15 +0800

    implement more options menu functions

--- ui/menu/options_audio.gd
+++ ui/menu/options_audio.gd
@@ -1,7 +1,21 @@
 extends TabBar
 
-@onready
-var id_sound_volume_ := AudioServer.get_bus_index("Music")
+@onready var id_music_volume_ := AudioServer.get_bus_index("Music")
+@onready var id_sound_volume_ := AudioServer.get_bus_index("Sound")
+
+@onready var slider_music_volume: HSlider = %slider_music_volume
+@onready var slider_sound_volume: HSlider = %slider_sound_volume
+
 
 func _on_slider_sound_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(id_sound_volume_, value)
+
+func _on_slider_music_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(id_music_volume_, value)
+
+func _on_button_reset_defaults_pressed() -> void:
+	AudioServer.set_bus_volume_linear(id_music_volume_, 1.0)
+	AudioServer.set_bus_volume_linear(id_sound_volume_, 1.0)
+	slider_music_volume.value = 1.0
+	slider_sound_volume.value = 1.0
+	
