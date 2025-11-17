36396e96a1906933e52a469d255fa74e25681a67
stereomatchingkiss <thamngapwei@gmail.com>
2025-11-17 15:05:31 +0800

    use list view to replace tab view, easier for gamepad to navigate

--- ui/menu/options_audio.gd
+++ ui/menu/options_audio.gd
@@ -6,7 +6,6 @@ extends TabBar
 @onready var slider_music_volume: HSlider = %slider_music_volume
 @onready var slider_sound_volume: HSlider = %slider_sound_volume
 
-
 func _on_slider_sound_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(id_sound_volume_, value)
 
@@ -18,4 +17,3 @@ func _on_button_reset_defaults_pressed() -> void:
 	AudioServer.set_bus_volume_linear(id_sound_volume_, 1.0)
 	slider_music_volume.value = 1.0
 	slider_sound_volume.value = 1.0
-	
