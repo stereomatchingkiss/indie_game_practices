5433c55ace5d9ad242c59fd71f0e2e248cd0dee4
stereomatchingkiss <thamngapwei@gmail.com>
2025-11-20 12:12:57 +0800

    simplify codes, easier to reuse

--- ui/menu/main_menu.gd
+++ ui/menu/main_menu.gd
@@ -4,15 +4,15 @@ extends Control
 @onready var button_game_start_: Button = %button_game_start
 @onready var button_options_: Button = %button_options
 
-@onready var original_normal_style_ = button_game_start_.get_theme_stylebox("normal")
-@onready var hover_style_ = button_game_start_.get_theme_stylebox("hover")
+@onready var original_normal_style_ : StyleBox = button_game_start_.get_theme_stylebox("normal")
+@onready var hover_style_ : StyleBox = button_game_start_.get_theme_stylebox("hover")
 
-@onready var theme_color_font_ = button_game_start_.get_theme_color("font_color")
-@onready var theme_color_hover_font_ = button_game_start_.get_theme_color("font_hover_color")
+@onready var theme_color_font_ : Color = button_game_start_.get_theme_color("font_color")
+@onready var theme_color_hover_font_ : Color = button_game_start_.get_theme_color("font_hover_color")
 
 @onready var buttons_array_ : Array[Button] = [button_game_start_, button_options_, button_end_]
 
-@onready var button_hover_ : int = 0
+@onready var menu_utils_: Node = %menu_utils
 
 enum ButtonName {
 	GAME_START,
@@ -30,11 +30,11 @@ func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
 			if event.keycode == KEY_ENTER:
-				if button_hover_ == ButtonName.GAME_END:
+				if menu_utils_.button_hover == ButtonName.GAME_END:
 					_on_button_end_pressed()
-				elif button_hover_ == ButtonName.GAME_START:
+				elif menu_utils_.button_hover == ButtonName.GAME_START:
 					_on_button_game_start_pressed()
-				elif button_hover_ == ButtonName.GAME_OPTIONS:
+				elif menu_utils_.button_hover == ButtonName.GAME_OPTIONS:
 					_on_button_options_pressed()
 			elif event.keycode == KEY_DOWN:
 				_process_key_down()
@@ -42,56 +42,41 @@ func _input(event):
 				_process_key_up()
 		
 func _ready() -> void:
-	button_hover_ = 0
-	_set_hovers(button_hover_)
+	menu_utils_.normal_style = original_normal_style_
+	menu_utils_.hover_style = hover_style_
+
+	menu_utils_.theme_color_font  = theme_color_font_
+	menu_utils_.theme_color_hover_font = theme_color_hover_font_
+		
+	menu_utils_.set_hovers(buttons_array_, menu_utils_.button_hover)
 	
 	print_debug(theme_color_font_, ":", theme_color_hover_font_)
 
+func _button_entered(bname : ButtonName) -> void:
+	menu_utils_.set_buttons_to_normal_style(buttons_array_)
+	menu_utils_.button_hover = bname
+	menu_utils_.set_hovers(buttons_array_, menu_utils_.button_hover)
+
 func _navigate_buttons_up_or_down(direction : NavigateDirection):
-	_set_buttons_to_normal_style()
+	menu_utils_.set_buttons_to_normal_style(buttons_array_)
 	if direction == NavigateDirection.DOWN:
-		if button_hover_ < (len(buttons_array_) - 1):
-			print_debug("down, set hover = ", button_hover_ + 1)
-			_set_hovers(button_hover_ + 1)
+		if menu_utils_.button_hover < (len(buttons_array_) - 1):
+			print_debug("down, set hover = ", menu_utils_.button_hover + 1)
+			menu_utils_.set_hovers(buttons_array_, menu_utils_.button_hover + 1)
 		else:
-			_set_hovers(ButtonName.GAME_START)
+			menu_utils_.set_hovers(buttons_array_, ButtonName.GAME_START)
 	else:
-		if button_hover_ > 0:
-			print_debug("up, set hover = ", button_hover_ - 1)
-			_set_hovers(button_hover_ - 1)
+		if menu_utils_.button_hover > 0:
+			print_debug("up, set hover = ", menu_utils_.button_hover - 1)
+			menu_utils_.set_hovers(buttons_array_, menu_utils_.button_hover - 1)
 		else:
-			_set_hovers(ButtonName.GAME_END)
+			menu_utils_.set_hovers(buttons_array_, ButtonName.GAME_END)
 
 func _process_key_down() ->void:
 	_navigate_buttons_up_or_down(NavigateDirection.DOWN)
 	
 func _process_key_up() ->void:
 	_navigate_buttons_up_or_down(NavigateDirection.UP)
-	
-func _set_buttons_to_normal_style():
-	for val in buttons_array_:
-		val.add_theme_stylebox_override("normal", original_normal_style_)
-		val.add_theme_stylebox_override("hover", original_normal_style_)
-		val.add_theme_color_override("font_color", theme_color_font_)
-		val.add_theme_color_override("font_hover_color", theme_color_font_)
-
-func _set_hovers(button_name : ButtonName) ->void:
-	for i in range(len(buttons_array_)):
-		if i == button_name:
-			buttons_array_[i].add_theme_stylebox_override("normal", hover_style_)
-			buttons_array_[i].add_theme_stylebox_override("hover", hover_style_)
-			
-			print_debug("change font color")
-			buttons_array_[i].add_theme_color_override("font_color", theme_color_hover_font_)
-			buttons_array_[i].add_theme_color_override("font_hover_color", theme_color_hover_font_)
-		else:
-			buttons_array_[i].add_theme_stylebox_override("normal", original_normal_style_)
-			buttons_array_[i].add_theme_stylebox_override("hover", original_normal_style_)
-			
-			buttons_array_[i].add_theme_color_override("font_color", theme_color_font_)
-			buttons_array_[i].add_theme_color_override("font_hover_color", theme_color_font_)
-	
-	button_hover_ = button_name
 
 func _on_button_game_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://stages/arena/1_0/graveyard_1_0.tscn")
@@ -103,19 +88,10 @@ func _on_button_end_pressed() -> void:
 	get_tree().quit()
 
 func _on_button_end_mouse_entered() -> void:
-	_set_buttons_to_normal_style()
-	#button_end_.add_theme_stylebox_override("hover", hover_style_)	
-	button_hover_ = ButtonName.GAME_END
-	_set_hovers(button_hover_)
+	_button_entered(ButtonName.GAME_END)
 
 func _on_button_options_mouse_entered() -> void:
-	_set_buttons_to_normal_style()
-	#button_options_.add_theme_stylebox_override("hover", hover_style_)
-	button_hover_ = ButtonName.GAME_OPTIONS
-	_set_hovers(button_hover_)
+	_button_entered(ButtonName.GAME_OPTIONS)
 
 func _on_button_game_start_mouse_entered() -> void:
-	_set_buttons_to_normal_style()
-	#button_game_start_.add_theme_stylebox_override("hover", hover_style_)	
-	button_hover_ = ButtonName.GAME_START
-	_set_hovers(button_hover_)
+	_button_entered(ButtonName.GAME_START)
